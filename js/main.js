1f4b83befea2c233b2b6501fbb0c49a71851782b
Ashley Radford <aradford7@outlook.com>
2019-04-10 19:48:39 -0700

    i tried to learn it mike and clay

--- js/main.js
+++ js/main.js
@@ -34,33 +34,121 @@ console.log('assessment is running?')
 // const add =  add;
 // const sub =  sub;
 // const input = document.getElementById('inputnum').val ();
-const add = document.getElementById('addbtn') 
-const sub = document.getElementById ('subbtn') 
-const input = document.getElementById ('inputnum')
+// const add = document.getElementById('addbtn') 
+// const sub = document.getElementById ('subbtn') 
+// const input = document.getElementById ('inputnum')
+// const num = document.getElementById('inputnum')
+// function display (){
+//     document.getElementById('displaynum').innerHTML('show', num)
+// }
 
-
-const num = document.getElementById('inputnum')
-
-
-
-function display (){
-    document.getElementById('displaynum').innerHTML('show', num)
-}
-
-function adding (){
-    let (num = 0, num + add, num++);{
-        return num
-    }
-}
-
-function subing (){
-    let (num = 0, num - sub, num--);{
-
-    }
-}
-// function calc(){
-//     if adding (let i += adding, i++);{
-//         return i;
+// function adding (){
+//     let (num = 0, num + add, num++);{////loop
+//         return num
 //     }
 // }
 
+// function subing (){
+//     let (num = 0, num - sub, num--);{////loop
+
+//     }
+// }
+
+//////////ABOVE IS WHERE I GAVE Up :(///////////////////////
+///Below my mentor is helping me understand Javascript :)/////
+
+//const add = document.getElementById('addbtn').innerHTML
+
+
+///CONSTANTS 1) set everything to dom 
+const $add = $('#addbtn')//console add jQuery.fn.init[button#addbtn]
+const $sub = $('#subbtn')//console sub jQuery.fn init[button#subbtn]
+const $display = $('#displaynum')//console display jQuery.fn.init [p#displaynum]
+const $inputValue = $('#userInput') //console check word input returns the div box where its in <input id = "input" type = "inputnum" value = "1">
+let num = 0 // the text number we are adding and subtracting from///renamed to let so we can change the value
+///remember to put $ infront of declared 'variables' that are dom elements
+//to check user input
+//in console right $(#userInput).val() 1 and it should be exactly like inputValue.val() get 1 too
+
+//BASICALLY WE ARE CHECKING EVERY OBJECTS IF IT CONNECT TO THEIR NEW VARIABLES!!! TO THEN USE DOM!
+//NOW BUILD FUNCTIONS TO MAKE IT DO SOMETHING
+
+//eventlisterner
+// document.getElementById('#addbtn').addEventListner('click', function)
+
+//since its declared already 
+//fx = x + 1  //read left to right
+$add.on('click',() => {
+
+   num += parseInt($inputValue.val())
+   update()
+});
+///in the console we can check the value of the variable named num by typing it in developertool
+// has to use parseInt() bc the 0 is read as a string so it converts it into a number
+$sub.on('click',() => {
+    num -= parseInt($inputValue.val())
+    update()
+ });
+/*
+let word= "500"
+undefined
+word
+"500"
+parseInt(word)
+500
+parseInt("400")
+400
+word
+"500"
+word+400
+"500400"
+parseInt(word)
+500
+parseInt(word)+400
+900
+
+*/
+//we put it outside of the scope we can use this function again for the subbtn and addbtn
+function update(){
+$display.text(num)
+}
+
+//// DOM.text() will print out the current text of the dom 
+
+
+//OKAY MULTIPLation and DIVISION W/O HELP! 
+
+const $mulitple = $('#multi');
+const $division = $('#divi');
+
+
+//event lister
+$mulitple.on('click',() => {
+    num *= parseInt($inputValue.val())
+    update();
+});
+//fx = x + 1
+
+$division.on('click', () => {
+    num /= parseInt($inputValue.val())
+    update();
+});
+
+
+///ASHLEY GODDARNIT REMEMBER HASHTAGS!!!! OR DOT REMEMBER TO ASSIGN DOM MULTIPLCATION!!!!!!!
+
+////set dom for add        x
+////set dom for subtract   x
+///set dom for display     x
+//set dom for input form   x
+
+
+//create event listener for add dom
+    ///add input to display
+        //grab display dom and add the input form dom
+            ////update the display
+
+//create event listener for subtract dom
+    ///subtract input from display
+            //grab display dom and subtract the input form dom
+                ////update the display
