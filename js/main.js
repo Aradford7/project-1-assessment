7b390f4fbc43a004d34c41ff69563df35bf9be5a
Ashley Radford <aradford7@outlook.com>
2019-04-10 17:07:33 -0700

    inital commit

---
+++ js/main.js
@@ -0,0 +1,66 @@
+console.log('assessment is running?')
+
+// need a event listner for the add button  ( on click do addition of w/e number is inputed)
+//need event listerner for subtract button ( onclick do subtraction of w/e number is inputed)
+//input element listens to input
+// display console by using object
+
+//const inputnum = num;
+
+// const inputValue =  document.getElementById('form').value ();
+//     document.getElementById('form').addEventListener('submit', function(e){
+//     //target and stop page from refreshing the info
+//     let target =  e.target;
+//     let preventDefault = e.preventDefault();
+
+//     console.log('form sub?');
+
+//     //retriving the value from the input
+// const inputValue = document.getElementById('inputnum').value ();
+//         document.getElementsById('display').innerHTML = console.log(inputValue)
+
+
+// });
+
+// add and sub function
+
+// document.getElementById("add").addEventListener("click", addnum);
+
+// function addnum(){
+//     document.getElementById("form").innerHTML  = 
+// }
+
+
+// const add =  add;
+// const sub =  sub;
+// const input = document.getElementById('inputnum').val ();
+const add = document.getElementById('addbtn') 
+const sub = document.getElementById ('subbtn') 
+const input = document.getElementById ('inputnum')
+
+
+const num = document.getElementById('inputnum')
+
+
+
+function display (){
+    document.getElementById('displaynum').innerHTML('show', num)
+}
+
+function adding (){
+    let (num = 0, num + add, num++);{
+        return num
+    }
+}
+
+function subing (){
+    let (num = 0, num - sub, num--);{
+
+    }
+}
+// function calc(){
+//     if adding (let i += adding, i++);{
+//         return i;
+//     }
+// }
+
